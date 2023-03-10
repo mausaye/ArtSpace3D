1ad0fdaf11008f189bf346498e5e446075198a4d
Tammy Lin <tammy@MacBook-Air-125.local>
2023-03-10 02:24:23 -0500

    cube and cone

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   var render = new ShapeRenderer();
 
   render.addCube(1, 1, 1, (0,0,0));
+  render.addCone(1,2,32, (0,0,0));
 
   render.renderObjects();
   console.log("hi");
--- src/ArtSpaceApplication/ShapeRenderer.js
+++ src/ArtSpaceApplication/ShapeRenderer.js
@@ -1,20 +1,23 @@
 
-import {PointLight,AmbientLight,Scene,Mesh,MeshBasicMaterial,WebGLRenderer,BoxGeometry, PerspectiveCamera, Vector3} from 'three';
+import {ConeGeometry,AmbientLight,MeshPhongMaterial,Scene,Mesh,MeshBasicMaterial,WebGLRenderer,BoxGeometry, PerspectiveCamera, Vector3} from 'three';
 
 export class ShapeRenderer{
-   scene;
-   sceneObjects;
-   renderer;
-   camera;
+    scene;
+    sceneObjects;
+    renderer;
+    camera;
 
   constructor(){
       this.scene = new Scene();
+
+      const light = new AmbientLight( 0x404040 );
+      this.scene.add(light);
+
       const fieldOfView = 75;
       const aspect = window.innerWidth / window.innerHeight;
   
       this.camera = new PerspectiveCamera(fieldOfView, aspect);
-      this.camera.lookAt(0,0,0);
-  
+     
       this.sceneObjects = [];
       this.renderer = new WebGLRenderer();
       this.renderer.setSize(window.innerWidth * 2, window.innerHeight, false);
@@ -22,29 +25,44 @@ export class ShapeRenderer{
    
   }
 
-
-  addCube(width, height, depth, position){
+  addCone(radius, height, radialSegments, position, color){
     // const width = 1;
     // const height = 1;
     // const depth = 1;
-    const geometry = new BoxGeometry(1, 1, 1);
+    const geometry = new ConeGeometry(radius, height, radialSegments);
     const material = new MeshBasicMaterial({ color: 0xff0000 });
+    const cone = new Mesh(geometry, material);
+
+    cone.position.z = -10;
+    cone.position.x = 3;
+    cone.rotation.x = 0;
+    cone.rotation.y = 5;
+
+    this.sceneObjects.push(cone);
+    
+  }
+
+  addCube(width, height, depth, position, color){
+    // const width = 1;
+    // const height = 1;
+    // const depth = 1;
+    const geometry = new BoxGeometry(width, height, depth);
+    const material = new MeshPhongMaterial({ color: 0xff0000 });
     const cube = new Mesh(geometry, material);
 
-    cube.position.z = -5;
+    cube.position.z = -10;
     cube.rotation.x = 10;
     cube.rotation.y = 5;
 
     this.sceneObjects.push(cube);
     
-    
-    
   }
   
   renderObjects(){
+    console.log(this.sceneObjects.length);
     for(var element of this.sceneObjects){
-     // this.scene.add(element);
-      console.log(element);
+      this.scene.add(element);
+      
     }
 
     this.renderer.render(this.scene, this.camera);
